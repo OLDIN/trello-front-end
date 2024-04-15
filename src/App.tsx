adf82ef262548e389b97b935f1ca294d7c90b3ac
Vladyslav <shilovladshilo@gmail.com>
2024-04-15 20:15:10 +0300

    fix: theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {
+  CssBaseline,
+  GlobalStyles,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material';
+
 import './App.scss';
 import Routers from './routes/Routes';
 import Header from './components/Header/Header';
+import { orange } from '@mui/material/colors';
+
+const theme = createTheme({
+  // status: {
+  //   danger: orange[500],
+  // },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: 'auto' } }} />
       <Header />
       <Routers />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
