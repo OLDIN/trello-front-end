e0598a435d4f672c773fd8503e84ee3bc1c39c2d
Vladyslav <shilovladshilo@gmail.com>
2024-04-25 17:16:43 +0300

    feat: add custom popover with predefined styles

---
+++ src/components/Popover/Popover.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import { PopoverBody, PopoverTitle } from './styles';
+
+import CloseIcon from '@mui/icons-material/Close';
+import InfoIcon from '@mui/icons-material/Info';
+import {
+  Grid,
+  IconButton,
+  Popover as PopoverBase,
+  PopoverOrigin,
+  Typography,
+} from '@mui/material';
+
+interface PopoverProps {
+  id?: string;
+  openPopover: boolean;
+  anchorEl: HTMLElement | null;
+  onClose: () => void;
+  children: React.ReactNode;
+  isDisabledClosing?: boolean;
+  title: string;
+  titleIcon?: React.ReactNode;
+  anchorOrigin?: PopoverOrigin;
+  transformOrigin?: PopoverOrigin;
+}
+
+export function Popover({
+  openPopover,
+  anchorEl,
+  onClose,
+  id,
+  isDisabledClosing,
+  children,
+  title,
+  titleIcon,
+  anchorOrigin = {
+    vertical: 'bottom',
+    horizontal: 'center',
+  },
+  transformOrigin = {
+    vertical: 'top',
+    horizontal: 'center',
+  },
+}: PopoverProps) {
+  const handleOnClose = () => {
+    if (isDisabledClosing) return;
+    onClose();
+  };
+
+  return (
+    <PopoverBase
+      open={openPopover}
+      anchorEl={anchorEl}
+      onClose={handleOnClose}
+      id={id}
+      anchorOrigin={anchorOrigin}
+      transformOrigin={transformOrigin}
+      sx={{
+        marginTop: '10px',
+      }}
+    >
+      <PopoverBody container direction="column">
+        <Grid item container justifyContent="space-between" alignItems="center">
+          <Grid item>{titleIcon}</Grid>
+          <Grid item>
+            <PopoverTitle variant="subtitle1">{title}</PopoverTitle>
+          </Grid>
+          <Grid item>
+            <IconButton
+              aria-label="close"
+              onClick={handleOnClose}
+              sx={{
+                position: 'absolute',
+                right: 5,
+                top: 5,
+                color: (theme) => theme.palette.grey[500],
+              }}
+              disabled={isDisabledClosing}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Grid item>{children}</Grid>
+      </PopoverBody>
+    </PopoverBase>
+  );
+}
---
+++ src/components/Popover/styles.ts
@@ -0,0 +1,27 @@
+import { Grid, styled, Typography } from '@mui/material';
+
+export const PopoverBody = styled(Grid)`
+  width: 304px;
+  padding: 8px 16px;
+`;
+
+export const PopoverTitle = styled(Typography)`
+  box-sizing: border-box;
+  color: #44546f;
+  display: block;
+  font-size: 14px;
+  font-weight: 600;
+  grid-column: 1 / span 3;
+  grid-row: 1;
+  height: 40px;
+  letter-spacing: -0.003em;
+  line-height: 16px;
+  line-height: 40px;
+  margin: 0;
+  overflow: hidden;
+  padding: 0 32px;
+  position: relative;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  z-index: 1;
+`;
